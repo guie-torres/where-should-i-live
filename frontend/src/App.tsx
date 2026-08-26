ab7ee08a8b4efbdaadd130e8a6cd3fcecdac36ae
Guillermo Torres <vilem.veze@gmail.com>
2026-08-26 12:36:42 +0200

    Add country comparision page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 import HomeCountrySelect from './pages/HomeCountrySelect/HomeCountrySelect'
 import Selection from './pages/Selection/Selection';
+import CountryComparision from './pages/CountryComparison/CountryComparison';
 import type { Country } from './countries/countries';
 function App() {
-const [homeCountry, setHomeCountry] = useState<Country | null>(null);
-const [preferedCountry, setPreferedCountry] = useState<Country | null>(null);
-  return(
-    <div>
-    {homeCountry == null ? 
-    <HomeCountrySelect setHomeCountry={setHomeCountry}/> 
-    : 
-    <Selection homeCountry={homeCountry} setHomeCountry={setHomeCountry} preferedCountry={preferedCountry} setPreferedCountry={setPreferedCountry}/>}
-    </div>
-  )
+  const [homeCountry, setHomeCountry] = useState<Country | null>(null);
+  const [preferedCountry, setPreferedCountry] = useState<Country | null>(null);
+
+  if(preferedCountry && homeCountry)
+    return <CountryComparision homeCountry={homeCountry} preferedCountry={preferedCountry}/>
+
+ if(!homeCountry)
+  return <HomeCountrySelect setHomeCountry={setHomeCountry}/> 
+
+ if (!preferedCountry)
+    return <Selection homeCountry={homeCountry} setHomeCountry={setHomeCountry} preferedCountry={preferedCountry} setPreferedCountry={setPreferedCountry}/>
 }
 
 export default App 
